87401f98e92049ee45e234413cb8b925a0659d71
Geovanny <geoconsultores.eu@gmail.com>
2022-03-04 21:18:04 -0500

    Abfragen2

--- bienen.sql
+++ bienen.sql
@@ -2,3 +2,13 @@ CREATE EXTENSION postgis;
 
 SELECT genus, family, COUNT(DISTINCT scientific) AS artenzahl
 FROM bienen GROUP BY genus, family ORDER BY artenzahl DESC;
+
+SELECT species, genus, COUNT(DISTINCT id) AS reg
+FROM bienen GROUP BY species, genus ORDER BY reg DESC;
+
+SELECT species AS arten, COUNT(DISTINCT id) AS reg
+FROM bienen WHERE genus = 'Bombus' GROUP BY arten ORDER BY reg DESC;
+
+SELECT year AS jahr, COUNT(DISTINCT species) AS arten_jahr, 
+COUNT(DISTINCT id) AS reg
+FROM bienen GROUP BY year ORDER BY year DESC, arten_jahr, reg;
